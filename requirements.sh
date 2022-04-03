dd83e05b259ecc117df871bf3713a2d9f8232448
Jitesh <46092054+jiteshsaini@users.noreply.github.com>
2022-04-04 01:02:17 +0530

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,7 @@
+echo "******Installing Tensorflow Lite and USB Coral Libraries for Raspberry Pi*******"
+
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install python3-tflite-runtime -y
+sudo apt-get install libedgetpu1-std -y
